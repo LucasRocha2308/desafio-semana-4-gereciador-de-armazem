075a95c35189c4cf67bddfbac6c09f5315438366
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2022-05-13 12:33:11 -0300

    commit inicial

---
+++ src/main/java/br/com/semana4_gerenciador_de_armazem/App.java
@@ -0,0 +1,13 @@
+package br.com.semana4_gerenciador_de_armazem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/semana4_gerenciador_de_armazem/AppTest.java
@@ -0,0 +1,20 @@
+package br.com.semana4_gerenciador_de_armazem;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
